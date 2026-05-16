68e5926e10a66e5c6c9c361fea1e544ed8989809
Marcos Raul Gutierrez Montes <marcos.gutierrez.montesal@iespsur.org>
2026-05-16 18:53:44 +0200

    FUNDAMENTOS

---
+++ PHP FUNDAMENTOS/variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php  
+        //variables
+        $edad = 20;
+        $estatura = 1.75;
+        $nombre = "Marcos";
+        $frases = ["$nombre tiene $edad años"];
+        $profesor = true;
+        
+        echo $edad;
+        echo "<br>";
+        echo $estatura;
+        echo "<br>";
+        echo "Tu nombre es " . $nombre;
+        echo $profesor;
+    ?>
+
+</body>
+</html>

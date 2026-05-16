b60165b39a6633505cdb2ad243a219491eba3613
Marcos Raul Gutierrez Montes <marcos.gutierrez.montesal@iespsur.org>
2026-05-16 18:37:17 +0200

    Hola_mundoyComentarios.php

---
+++ Hola_mundoyComentarios.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php echo "Hola Mundo"; 
+    //soy un comentario de una sola linea
+    /*soy un comentario
+     de varias 
+     lineas
+    */
+    # Comentario de tipo linux
+    ?>
+
+</body>
+</html>

4b4ce695db7b072e57d0f5c6d178fd835bfbbb4a
Marcos Raul Gutierrez Montes <marcos.gutierrez.montesal@iespsur.org>
2026-05-16 21:02:44 +0200

    FUNDAMENTOS

---
+++ PHP FUNDAMENTOS/Include, require y goto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php  
+// Include
+
+echo "Soy el primer fichero";
+echo "<br>";
+include "Include, require y goto_segundo fichero.php";
+echo "<br>";
+include "Include, require y goto_segundo fichero.php";
+    ?>
+
+</body>
+</html>

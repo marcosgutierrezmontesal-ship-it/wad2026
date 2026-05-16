8d5dfa9b931345630f8bedc64faafc32f6ee16ce
Marcos Raul Gutierrez Montes <marcos.gutierrez.montesal@iespsur.org>
2026-05-16 22:03:47 +0200

    FUNDAMENTOS

---
+++ PHP FUNDAMENTOS/Modificar arrays_part2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php  
+        //Funciones para modificar arrays
+
+$frutas = array("naranja","plátano","manzana","frambuesa");
+
+var_dump($frutas);
+echo "<br>";
+
+$eliminado = array_shift($frutas);
+var_dump($eliminado);
+echo "<br>";
+var_dump($frutas);
+
+array_unshift($frutas,"pera");
+echo "<br>";
+var_dump($frutas);
+    ?>
+
+</body>
+</html>
---
+++ PHP FUNDAMENTOS/Modificar arrays_part3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php  
+        //Funciones para modificar arrays
+
+$frutas = array("naranja","plátano","manzana","frambuesa");
+
+var_dump($frutas);
+echo "<br>";
+
+$eliminado = array_pop($frutas);
+
+var_dump($eliminado);
+echo "<br>";
+
+var_dump($frutas);
+echo "<br>";
+
+array_push($frutas, "pera");
+
+var_dump($frutas);
+echo "<br>";
+    ?>
+
+</body>
+</html>

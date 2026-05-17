e3728688884d75e409ceb32d62d61642373bc8e9
Marcos Raul Gutierrez Montes <marcos.gutierrez.montesal@iespsur.org>
2026-05-17 19:02:01 +0200

    PHP: Ampliando Conceptos

---
+++ PHP Ampliando Conceptos/Varias funciones de números_part1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP: Ampliando Conceptos</title>
+</head>
+<body>
+
+    <?php
+    //Funciones variadas para números
+
+echo(abs(-6.7));
+echo "<br>";
+echo(max(2,4,6,8,10));
+echo "<br>";
+echo(min(2,4,6,8,10));
+echo "<br>";
+
+echo(rand());
+echo "<br>";
+
+echo(rand(10,100));
+echo "<br>";
+
+echo(sqrt(9));
+    ?>
+
+</body>
+</html>
---
+++ PHP Ampliando Conceptos/Varias funciones de números_part2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP: Ampliando Conceptos</title>
+</head>
+<body>
+
+    <?php
+    //Funciones variadas para números
+
+    echo decbin("3");
+echo "<br>";
+echo decbin("1587");
+echo "<br>";
+
+echo dechex("10");
+echo "<br>";
+echo bindec("1011");
+    ?>
+
+</body>
+</html>

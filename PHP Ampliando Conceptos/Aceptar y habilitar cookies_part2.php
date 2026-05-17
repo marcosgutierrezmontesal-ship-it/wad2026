690486442958f915454b7c640fc1da472ac5d0b6
Marcos Raul Gutierrez Montes <marcos.gutierrez.montesal@iespsur.org>
2026-05-17 19:32:19 +0200

    PHP: Ampliando Conceptos

---
+++ PHP Ampliando Conceptos/Aceptar y habilitar cookies_part2.php
@@ -0,0 +1,20 @@
+<?php
+
+if(count($_COOKIE) > 0){
+        echo "Cookies habilitadas";
+}
+else{
+        echo "Cookies deshabilitadas";
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP: Ampliando Conceptos</title>
+</head>
+<body>
+
+</body>
+</html>
